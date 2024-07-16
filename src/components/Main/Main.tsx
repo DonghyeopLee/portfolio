6fc16aa41a349e11c9af0433cddcf664a4c884c7
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-07-17 04:01:50 +1000

    asd

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,8 +4,8 @@ import { Hero } from "../Hero/Hero";
 import { About } from "../About/About";
 import { Contact } from "../Contact/Contact";
 import { Project } from "../Project/Project";
-import wordpress from "../../assets/wordpress.svg";
-import shopify from "../../assets/shopify.svg";
+import wordpress from "../../assets/python.svg";
+import shopify from "../../assets/java.svg";
 import htmlIcon from "../../assets/html-icon.svg";
 import cssIcon from "../../assets/css-icon.svg";
 import sassIcon from "../../assets/sass-icon.svg";
@@ -13,7 +13,7 @@ import jsIcon from "../../assets/js-icon.svg";
 import nodeIcon from "../../assets/node-icon.svg";
 import reactIcon from "../../assets/react-icon.svg";
 import typescriptIcon from "../../assets/typescript-icon.svg";
-import vueIcon from "../../assets/vue-icon.svg";
+import vueIcon from "../../assets/aws.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
 
 export function Main() {
