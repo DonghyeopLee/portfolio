0f091402db70fd405fe28dfc8f902a59f5d7ab5c
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-10-04 22:43:49 +1000

    update

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -16,7 +16,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://github.com/DonghyeopLee/RestAPI" target="_blank" rel="noreferrer">
+                <a href="https://github.com/DonghyeopLee/banking-app" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
                 <a href="" target="_blank" rel="noreferrer">
