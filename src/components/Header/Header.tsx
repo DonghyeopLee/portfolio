ba19084d8ac13123e2da0814476bf326275dd52c
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2025-01-04 02:11:00 +1100

    12

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Donghyeop Lee_CV .pdf'
+import Resume from '../../assets/Ian_Lee_Resume.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
